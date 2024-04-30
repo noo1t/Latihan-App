31a9c3919dd4a6bf9c662b0765f34a6fdf0bbe12
unknown <emyr@students.amikom.ac.id>
2024-04-30 10:22:19 +0700

    first commit

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+<h2>Tambahkan Biodata Anda</h2>
+@section('title', 'Biodata Karyawan')
+
+@section('sidebar')
+    @parent
+    <p>This is appended to the master sidebar.</p>
+@endsection
+
+@section('content')
+    <p>Sistem informasi (SI) adalah sebuah sistem formal, sosioteknikal, dan organisasional yang dirancang untuk mengumpulkan, memproses, menyimpan, dan mendistribusikan informasi.[1] Dari perspektif sosioteknis, sistem informasi disusun oleh empat komponen: tugas, orang, struktur (atau peran), dan teknologi.[2] Sistem informasi dapat didefinisikan sebagai suatu integrasi komponen untuk pengumpulan, penyimpanan dan pemrosesan data. Data tersebut kemudian digunakan untuk menyediakan informasi, berkontribusi pada pengetahuan serta produk digital yang memfasilitasi pengambilan keputusan.[3]
+
+        Sistem informasi komputer adalah sistem yang terdiri dari manusia dan komputer yang memproses atau menafsirkan informasi.[4][5][6][7] Istilah ini juga terkadang digunakan untuk merujuk pada sistem komputer dengan perangkat lunak yang diinstal.
+        
+        Sistem informasi dapat digunakan dalam berbagai konteks, termasuk bisnis, pendidikan, pemerintahan, kesehatan, dan banyak lagi.
+        
+        "Sistem informasi" juga merupakan bidang studi akademik tentang sistem dengan referensi khusus untuk informasi dan jaringan pelengkap perangkat keras dan perangkat lunak komputer yang digunakan orang dan organisasi untuk mengumpulkan, menyaring, memproses, membuat, dan juga mendistribusikan data.[8] Dengan penekanan pada sistem informasi yang memiliki batas definitif, pengguna, prosesor, penyimpanan, input, output, dan jaringan komunikasi tersebut.[9]</p>
+@endsection
---
+++ resources/views/hello.blade.php
@@ -0,0 +1,28 @@
+<!-- hello.blade.php -->
+<h2>Selamat Datang</h2>
+<b>Semoga sukses dan lanjar belajar Laravel Framework 10.x</b>
+
+<form method="POST" action="">
+    <table>
+        <tr>
+            <td>NIM</td>
+            <td>:</td>
+            <td><input type="text" name="nim" value="" /></td>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td>:</td>
+            <td><input type="text" name="nama" value="" /></td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>:</td>
+            <td><input type="text" name="email" value="" /></td>
+        </tr>
+        <tr>
+            <td><input type="submit" name="btn_submit" value="Simpan" /></td>
+            <td></td>
+            <td><input type="reset" name="btn_cancel" value="Batal" /></td>
+        </tr>
+    </table>
+</form>
